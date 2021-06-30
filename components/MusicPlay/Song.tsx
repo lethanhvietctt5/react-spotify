6e9f3e3356ad668ca95743baa80693b050e6bec0
Le Thanh Viet <lethanhviet7c@gmail.com>
2021-06-30 23:49:01 +0700

    fix UI and change Scopes

--- components/MusicPlay/Song.tsx
+++ components/MusicPlay/Song.tsx
@@ -6,7 +6,7 @@ export default function Song() {
   const state = useAppSelector((state) => state.player);
   return (
     <div className="w-1/3 lg:w-1/4 min-w-min h-full text-white flex items-center pl-4">
-      <div className="2xl:p-3 w-1/5">
+      <div className="2xl:p-3 w-1/5 mr-3">
         <Image
           layout="responsive"
           height="64"
