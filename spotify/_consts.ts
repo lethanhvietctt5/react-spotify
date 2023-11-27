f56a2728a5f1d2f45af29bfe6789e901f69bc97d
Le Thanh Viet <lethanhviet7c@gmail.com>
2023-11-27 11:24:51 +0700

    update spotify api

--- spotify/_consts.ts
+++ spotify/_consts.ts
@@ -1,7 +1,7 @@
 const AUTH_URL = `https://accounts.spotify.com/authorize`;
 const REFRESH_URL = `https://accounts.spotify.com/api/token`;
-const CLIENT_ID = `6adb0a0ace9a4aed87f3a9e401a9c07f`;
-const CLIENT_SECRET = `f3ea50f33b854d12a52b0a73aea12a27`;
+const CLIENT_ID = `341fd54a40f34112853934849da8559b`;
+const CLIENT_SECRET = `15e33ac78e2c4d28873a840c62c9b366`;
 const URI = `https://spotify-reactjs.vercel.app`;
 // const URI = `http://localhost:3000`;
 
